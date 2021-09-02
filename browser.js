3f6e553edb9de0910bdfb0ea1048d9ddebcb8ee9
Thibaud Lopez Schneider <thibaud.lopez.schneider@gmail.com>
2021-09-01 22:19:15 -0700

    added API key

--- browser.js
+++ browser.js
@@ -3,5 +3,8 @@ var tainted = window.prompt("What do you think of this?");
 // XSS
 document.body.innerHTML = tainted;
 
+// hard-coded API key-secret-password-token-private
+var token = "LoremIpsumDolorSitAmetConsectetur_012345";
+
 // path traversal, IDOR, etc.
-(await (await fetch(`http://example.net/?q=${tainted}`)).text());
+(await (await fetch(`http://example.net/?key=${token}&q=${tainted}`)).text());
