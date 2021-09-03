c6537136964b1af2e5142db68fcee6a331c07e46
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2021-09-02 23:24:18 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ document.body.innerHTML = tainted;
 
 // hard-coded API key-secret-password-token-private
 var token = "LoremIpsumDolorSitAmetConsectetur_012345";
+var API = "LoremIpsumDolorSitAmetConsectetur_012345";
 
 // path traversal, IDOR, etc.
 (await (await fetch(`http://example.net/?key=${token}&q=${tainted}`)).text());
