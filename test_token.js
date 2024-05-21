43c567d73972ea428274b19bc1b39266afdf9dc8
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2024-05-21 10:40:06 -0700

    Update test_token.js

--- test_token.js
+++ test_token.js
@@ -1,4 +1,4 @@
 // test of hard-coded token
 // const base64url = require('base64url');
-// ['sk', base64url(JSON.stringify({u:'foo',a:'bar'})), base64url('helloworld')].join('.')
+// ['sk', base64url(JSON.stringify({u:'foo',a:'bar'})), base64url('helloworld')].join('.');
 const access_token = 'sk.eyJ1IjoiZm9vIiwiYSI6ImJhciJ9.aGVsbG93b3JsZA';
